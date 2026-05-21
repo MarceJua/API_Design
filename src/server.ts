9c394faf413e35163c0a24f531ba968044166abc
MarceJua <mjuarez2017ig@gmail.com>
2026-05-21 09:57:14 -0600

    feat: implement validation middleware for request body, parameters, and queries; add habit and user routes with validation

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,28 @@
 import express from 'express'
 import authRoutes from './routes/authRoutes.ts'
 import userRoutes from './routes/userRoutes.ts'
-import habitRoutes from './routes/habirRoutes.ts'
+import habitRoutes from './routes/habitRoutes.ts'
+import cors from 'cors'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import {  env, isDev, isTestEnv } from '../env.ts'
 
 const app = express()
+app.use(helmet())
+app.use(
+  cors({
+    origin: env.CORS_ORIGIN,
+    credentials: true,
+  })
+)
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(
+  morgan('dev', {
+    skip: () => isTestEnv(),
+  })
+)
 
 // Health check endpoint
 app.get('/health', (req, res) => {
@@ -12,6 +31,7 @@ app.get('/health', (req, res) => {
     timestamp: new Date().toISOString(),
     service: 'Habit Tracker API',
   })
+  // res.send('<button>click </button>')
 })
 
 app.use('/api/auth', authRoutes)
