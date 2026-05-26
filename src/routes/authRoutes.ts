d266fa386eceecb4520cb365977478a77c442d5c
MarceJua <mjuarez2017ig@gmail.com>
2026-05-26 17:34:20 -0600

    feat: implement user registration with password hashing and JWT generation; add auth routes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
+import { register } from '../controllers/authController.ts'
+import { validateBody } from '../middleware/validation.ts'
+import { z } from 'zod'
+import { insertUserSchema } from '../db/schema.ts'
 
 const router = Router()
 
-router.post('/register', (req, res) => {
-  res.status(201).json({ message: 'User registered successfully' })
-})
+router.post('/register', validateBody(insertUserSchema), register)
 
 router.post('/login', (req, res) => {
   res.status(200).json({ message: 'User logged in successfully' })
